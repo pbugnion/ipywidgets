d654135ea8a2fecdf2e6ae35ee59e2e6548fe142
Pascal Bugnion <pascal@bugnion.org>
2017-10-13 18:36:56 +0100

    Unit tests for bounded integer textbox

--- ipywidgets/widgets/tests/test_widget_int.py
+++ ipywidgets/widgets/tests/test_widget_int.py
@@ -3,7 +3,7 @@
 
 from unittest import TestCase
 
-from ipywidgets import IntText
+from ipywidgets import IntText, BoundedIntText
 
 
 class TestIntText(TestCase):
@@ -28,3 +28,37 @@ class TestIntText(TestCase):
         textbox = IntText()
         textbox.value = 72
         assert textbox.get_state()['value'] == 72
+
+
+class TestBoundedIntText(TestCase):
+
+    def test_construction_default_value(self):
+        textbox = BoundedIntText()
+        state = textbox.get_state()
+        assert state['value'] == 0
+        assert state['disabled'] is False
+        assert state['continuous_update'] is False
+        assert state['min'] == 0
+        assert state['max'] == 100
+        assert state['step'] == 1
+
+    def test_construction_explicit_arguments(self):
+        textbox = BoundedIntText(52, -5, 81, 3)
+        state = textbox.get_state()
+        assert state['value'] == 52
+        assert state['min'] == -5
+        assert state['max'] == 81
+        assert state['step'] == 3
+
+    def test_construction_with_kwargs(self):
+        textbox = BoundedIntText(
+            value=52,
+            min=-5,
+            max=81,
+            step=3
+        )
+        state = textbox.get_state()
+        assert state['value'] == 52
+        assert state['min'] == -5
+        assert state['max'] == 81
+        assert state['step'] == 3
