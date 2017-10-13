1ec7afe001e9d45571426c08807a2b2e984db2a2
Pascal Bugnion <pascal@bugnion.org>
2017-10-13 18:46:31 +0100

    Remove unnecessary injection of argument
    The _bounded_int_doc function injects the docstring and also moves the value
    argument into the kwargs before passing the kwargs to the base widget
    constructor. This is unnecessary, since we only use _bounded_int_doc to decorate
    classes that inherit from _BoundedInt, which also does this.

--- ipywidgets/widgets/widget_int.py
+++ ipywidgets/widgets/widget_int.py
@@ -43,19 +43,7 @@ def _int_doc(cls):
 
 def _bounded_int_doc(cls):
     """Add bounded int docstring template to class init."""
-    def __init__(self, value=None, min=None, max=None, step=None, **kwargs):
-        if value is not None:
-            kwargs['value'] = value
-        if min is not None:
-            kwargs['min'] = min
-        if max is not None:
-            kwargs['max'] = max
-        if step is not None:
-            kwargs['step'] = step
-        super(cls, self).__init__(**kwargs)
-
-    __init__.__doc__ = _bounded_int_doc_t
-    cls.__init__ = __init__
+    cls.__doc__ = _bounded_int_doc_t
     return cls
 
 
